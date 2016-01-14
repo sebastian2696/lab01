d614a136f08b5e2c3a3617210962e1d466e40df4
sebastian2696 <sebastian2696@gwu.edu>
2016-01-13 23:28:31 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,5 +7,18 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello world!\n");
-	return 0;
+	/*return 0;*/
+	/* This section computes a sum. */
+	int x, y, z;
+	x = 10;
+	y = 20;
+	z = x + y;
+	printf("x is %d, y is %d, and z is %d\n", x, y, z);
+	/* This sections computes the volume of a box.*/
+	int q;
+	x = 11;
+	y = 14;
+	z = 7;
+	q = x*y*z;
+	printf("The height is %d, the width is %d, the depth is %d and the volume of the box is %d\n", x, z, y, q);
 }
